05cf9775dd1f7f1ea24db6e78cfdea9f0bea6b37
laasya1302 <laasyakantamreddy@gmail.com>
2026-02-05 15:47:06 +0530

    practiced

---
+++ day-08/script.js
@@ -0,0 +1,16 @@
+// let arr=[10,20,30,40];
+// console.log(arr);
+// let len=arr.length;
+// console.log(len);
+// console.log(arr[0]);
+// arr[0]=90;
+// console.log(arr[0]);
+// console.log(arr);
+// delete arr[2];
+// console.log(arr);
+// arr.push(80);
+// console.log(arr);
+
+
+let arr=["L","P","D","S","Y","C"];
+console.log(arr);
---
+++ day-09/script.js
@@ -0,0 +1,11 @@
+const obj=[
+    {
+        image:
+        name:"wireless headphones",
+        description:"high-quality bluetooth headphones with noise cancellation",
+        price:79.99,
+        stock:50,
+        tag:"electronics"
+    }
+]
+    
